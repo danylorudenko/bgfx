9b71cbbe74f6f6ab1ca8ac0b807ec6c68fe4afb2
Danylo Rudenko <rudenko.dany@gmail.com>
2019-10-01 22:42:29 +0300

    Improvements in shader script for putting output in right directory.

--- process_user_shaders.bat
+++ process_user_shaders.bat
@@ -1,4 +1,9 @@
-for /r %%v in (usercode\userproject0\shaders\vertex\*.*) do .build\win64_vs2017\bin\shadercDebug.exe --verbose -i .\src --varyingdef .\src\varying.def.sc --platform windows --profile "s_5" --type vertex -O 0 -f "%%v" -o "%%v.bin"
+rmdir /S /Q .\usercode\runtime\shaders
+mkdir .\usercode\runtime\shaders\vertex\
+mkdir .\usercode\runtime\shaders\fragment\
 
-for /r %%v in (usercode\userproject0\shaders\fragment\*.*) do .build\win64_vs2017\bin\shadercDebug.exe --verbose -i .\src --varyingdef .\src\varying.def.sc --platform windows --profile "s_5" --type fragment -O 0 -f "%%v" -o "%%v.bin"
+for /r %%V in (usercode\userproject0\shaders\vertex\*.*) do .build\win64_vs2017\bin\shadercDebug.exe --verbose -i .\src --varyingdef .\src\varying.def.sc --platform windows --profile "vs_5_0" --type vertex -O 0 -f "%%V" -o "usercode\runtime\shaders\vertex\%%~nV.bin"
+
+for /r %%V in (usercode\userproject0\shaders\fragment\*.*) do .build\win64_vs2017\bin\shadercDebug.exe --verbose -i .\src --varyingdef .\src\varying.def.sc --platform windows --profile "ps_5_0" --type fragment -O 0 -f "%%V" -o "usercode\runtime\shaders\fragment\%%~nV.bin"
+
 pause
