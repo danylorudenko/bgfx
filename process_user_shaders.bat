df65d19ce7bfec14b597b674c9b6fa86f2fad653
Danylo Rudenko <rudenko.dany@gmail.com>
2019-10-02 23:53:33 +0300

    Moved shader compilation script to the project it belongs to.

--- process_user_shaders.bat
+++
@@ -1,9 +0,0 @@
-rmdir /S /Q .\usercode\runtime\shaders
-mkdir .\usercode\runtime\shaders\vertex\
-mkdir .\usercode\runtime\shaders\fragment\
-
-for /r %%V in (usercode\userproject0\shaders\vertex\*.*) do .build\win64_vs2017\bin\shadercDebug.exe --verbose -i .\src --varyingdef .\src\varying.def.sc --platform windows --profile "vs_5_0" --type vertex -O 0 -f "%%V" -o "usercode\runtime\shaders\vertex\%%~nV.bin"
-
-for /r %%V in (usercode\userproject0\shaders\fragment\*.*) do .build\win64_vs2017\bin\shadercDebug.exe --verbose -i .\src --varyingdef .\src\varying.def.sc --platform windows --profile "ps_5_0" --type fragment -O 0 -f "%%V" -o "usercode\runtime\shaders\fragment\%%~nV.bin"
-
-pause
